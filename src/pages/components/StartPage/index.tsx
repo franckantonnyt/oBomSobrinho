708314e8cb8ef93a28a24cbbf7d8da900475c7e0
Franck <franckantonnyt@outlook.com>
2022-02-20 19:43:12 -0300

    add page responsive

--- src/pages/components/StartPage/index.tsx
+++ src/pages/components/StartPage/index.tsx
@@ -3,8 +3,6 @@ import { Link, animateScroll as scroll } from 'react-scroll';
 
 import styles from './styles.module.scss';
 
-import BannerBusiness from '../../../../public/assets/icons/banner_business.svg';
-
 const StartPage = () => {
   const [navBarColor, setNavBarColor] = useState(false);
   const changeBackgroundHeader = () => {
@@ -21,12 +19,10 @@ const StartPage = () => {
     }
   });
 
-
-
   return (
     <section className={styles.startPage}>
       <div className={styles.containerTitleButton}>
-        <h1>Quer Vender Mais ?</h1>
+        <h1>Quer<br />Vender<br />Mais ?</h1>
         <p>
           Aumente suas vendas na internet com nossa solução de marketing integrado, findelizando e conquistando mais clientes e fazer do seu negócio uma referência na distribuição do mercado. <b>Nossa missão é ajudar seu negócio a vender mais !</b>
         </p>
@@ -34,9 +30,8 @@ const StartPage = () => {
           <button>Quero saber como vender mais</button>
         </Link>
       </div>
-      <div className={styles.containerForm}>
-        <BannerBusiness />
-        <img src="assets/images/background_gradient_fluid.png" alt="background" />
+      <div className={styles.containerBackground}>
+        <img src="assets/images/background_gradient_fluid_business.png" alt="background" />
       </div>
     </section>
   );
