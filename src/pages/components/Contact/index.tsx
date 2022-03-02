1cd0720fbc5e0e5fac1366726b4ca6c07243327e
Franck <franckantonnyt@outlook.com>
2022-03-02 19:52:37 -0300

    add form

--- src/pages/components/Contact/index.tsx
+++ src/pages/components/Contact/index.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
+import { HiOutlineX } from 'react-icons/hi';
 
 import Form from '../Form';
+import IconWhatsapp from '../../../../public/assets/icons/icon-whatsapp.svg';
 
 const Contact = () => {
+  const [modal, setModal] = useState(false);
+
   return (
     <section className={styles.containerContact} id='/contato'>
       <div className={styles.containerForm}>
+        <div className={modal ? styles.showModal : styles.hideModal}>
+          <div>
+            <button type='button' onClick={() => setModal(false)}>
+              <HiOutlineX className={styles.closeIcon} />
+            </button>
+            <h1>Cadastro Efetuado!</h1>
+            <p>Em até 3 dias nossa equipe entrará em contato para agendar a consultoria.</p>
+            <a target="_blank" href="https://api.whatsapp.com/send?phone=5535984774753&text=Olá, gostaria de fazer uma consultoria agora." >Se quiser fazer uma consultoria agora
+              <IconWhatsapp />
+            </a>
+          </div>
+        </div>
         <section>
           <header>
             <h1>Receba nossa consultoria <b>gratuita</b> <br />e comece a vender mais na internet !</h1>
@@ -23,7 +40,7 @@ const Contact = () => {
             Cadastre seus dados e receba uma <strong>CONSULTORIA GRATUITA</strong> focada em trazer mais clientes qualificados com a internet e aumentar as vendas do seu negócio!
           </p>
         </section>
-        <section><Form /></section>
+        <section><Form clickButton={(e) => setModal(e)} /></section>
       </div>
       <footer className={styles.containerFooter}>
         <p>
--- src/pages/components/Form/index.tsx
+++ src/pages/components/Form/index.tsx
@@ -1,17 +1,38 @@
+import React, { useRef, useState } from 'react';
+import InputMask from 'react-input-mask';
+import emailjs from '@emailjs/browser';
 import styles from './styles.module.scss';
 
-const StartPage = () => {
+const Form = ({ clickButton }) => {
+    const form = useRef();
+    const [sendForm, setSendForm] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setSendForm(true);
+        emailjs.sendForm('gmailMessage', 'template_2o1veye', form.current, 'user_gIl675C0HvKoRsyaMH4kO')
+            .then((result) => {
+                console.log(result);
+            }, (error) => {
+                console.log(error);
+            });
+        setTimeout(() => {
+            setSendForm(false);
+            clickButton(!sendForm);
+        }, 1500);
+    };
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} ref={form} onSubmit={sendEmail}>
             <h1>Cadastre-se para agendar<br /> sua consultoria gratuita!</h1>
-            <input type="text" name="name" placeholder="Nome:" aria-required="true" />
-            <input type="email" name="email" placeholder="E-mail:" aria-required="true" />
-            <input type="text" name="phone" placeholder="DDD + Telefone:" aria-required="true" data-fme-mask="ev-tel-ddd9" />
-            <input type="text" name="place" placeholder="Empresa:" aria-required="true" />
+            <input type="text" name="name" placeholder="Nome:" />
+            <input type="email" name="email" placeholder="E-mail:" />
+            <InputMask placeholder="DDD + Telefone:" name="phone" mask="(99) 99999-9999" />
+            <input type="text" name="place" placeholder="Empresa:" />
             <input type="text" name="site" placeholder="Site: (Opcional)" />
-            <button onSubmit={(e) => e} type="submit">Receber consultoria gratuita</button>
+            <button className={sendForm && styles.desactive} onSubmit={(e) => e} type="submit">Receber consultoria gratuita</button>
         </form>
     );
 };
 
-export default StartPage;
+export default Form;
