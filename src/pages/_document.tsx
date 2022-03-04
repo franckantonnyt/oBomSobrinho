d2256b54653969437f01b01f01b66790ebc80dfb
Franck <franckantonnyt@outlook.com>
2022-03-04 11:47:56 -0300

    insert google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,28 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <!-- Google Tag Manager --> */}
+          <script type="text/javascript" dangerouslySetInnerHTML={{
+            __html: `(function (w, d, s, l, i) {
+            w[l] = w[l] || []; w[l].push({
+              'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+            }); var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-TS3S2N5');`}} />
+          {/* <!-- End Google Tag Manager --> */}
+
+          {/* <script type="text/javascript" dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2859144,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} /> */}
+
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href='https://fonts.googleapis.com/css2?family=Maven+Pro:wght@700;800;900&display=swap'
@@ -15,25 +37,19 @@ export default class MyDocument extends Document {
             rel='stylesheet'
           />
           <link rel='shortcut icon' href='/favicon.png' type='image/png' />
-
-          {/* <!-- Hotjar Tracking Code for https://obomsobrinho.com.br/ --> */}
-          <script>
-            {`(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2859144,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-            }
-          </script>
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS3S2N5"
+              height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
+            </iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
-      </Html>
+      </Html >
     );
   }
 }
