cbbfa8ff61ff6292d5ceaf6d595ae3934979f6ae
Franck <franckantonnyt@outlook.com>
2022-04-02 17:38:53 -0300

    removed facebook script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,17 +16,6 @@ export default class MyDocument extends Document {
               j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
           })(window, document, 'script', 'dataLayer', 'GTM-TS3S2N5');`}} />
-          {/* <!-- End Google Tag Manager --> */}
-
-          {/* <script type="text/javascript" dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:2859144,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} /> */}
 
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
